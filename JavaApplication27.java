ba1ba36cb267bb518ff8d559b12fb65d42efb532
Saad <mraihx2@gmail.com>
2019-07-06 02:59:58 +0300

    Create JavaApplication27.java
    test

---
+++ JavaApplication27.java
@@ -0,0 +1,20 @@
+
+
+package javaapplication27;
+
+import java.util.ArrayList;
+
+
+
+public class JavaApplication27 {
+
+  
+    
+    public static void main(String[] args) 
+    {
+       System.out.println(" Hi world ");
+       
+	
+    }
+    
+}
